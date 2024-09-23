e117b60a1176b6574ee2d3216181c2d470de2319
angieVentura <angiev.d.23@gmail.com>
2024-09-23 04:28:19 -0300

    CRUD fullstack: Spring Boot + React

---
+++ CRUD-fullstack/ecommerce/src/main/java/com/ecommerce/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce.repositories;
+
+
+import com.ecommerce.ecommerce.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
+}
